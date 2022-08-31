7abe1443db80e45f9096e43110a240c9586df769
Johan Mateo Rosero Quenguan <roseroq-j@javeriana.edu.co>
2022-08-31 12:38:18 -0500

    clases

---
+++ Library/src/main/java/com/example/Library/domain/Editorial.java
@@ -0,0 +1,5 @@
+package com.example.Library.domain;
+
+/* el manejo de esta clsae se hace para seguir con buenas practicas de programación*/
+public class Editorial {
+}
---
+++ Library/src/main/java/com/example/Library/domain/User.java
@@ -0,0 +1,4 @@
+package com.example.Library.domain;
+
+public class User {
+}
