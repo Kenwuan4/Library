8810c9c248b4fd79014c5992571c1e0c0fee8733
Johan Mateo Rosero Quenguan <roseroq-j@javeriana.edu.co>
2022-09-20 21:38:47 -0500

    microservicio de personal

---
+++ Proyecto/staff/src/main/java/com/example/staff/model/Staff.java
@@ -0,0 +1,43 @@
+package com.example.staff.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+public class Staff {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private long identification;
+
+    @Column
+    private Date birth;
+
+    @Column
+    private String role;
+
+    @Column
+    private boolean active;
+}
+
