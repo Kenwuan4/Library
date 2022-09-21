97ee0b9f07b231c1ce116872e7858f8781fdb220
Johan Mateo Rosero Quenguan <roseroq-j@javeriana.edu.co>
2022-09-21 12:17:53 -0500

    Cambio de archivos de microservicios
    se cambiaron los arcihvos por incompatibilidad de dependencias

---
+++ Proyecto/book/book/src/main/java/com/example/book/repository/BookRepository.java
@@ -0,0 +1,15 @@
+package com.example.book.repository;
+
+import com.example.book.model.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookRepository  extends JpaRepository<Book, Integer> {
+
+    Page<Book> findAll(Pageable pageable);
+    List<Book> findByEditorialId(Integer id);
+    List<Book> findBooksByPriceIsLessThanEqual(double price);
+}
